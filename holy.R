075ba07b831d7997748c9d715b667bed510d923d
Zack Chambers <zmchambers89@gmail.com>
2022-04-21 16:01:40 -0700

    added conditional statement

--- holy.R
+++ holy.R
@@ -1,7 +1,14 @@
 ## new file for testing
 
-
-
 newVar <- 'hello, github'
 
 secondVar <- "kinda worked"
+
+if (secondVar == "kinda worked") {
+  
+  print("Yes, indeed")
+} else {
+  
+  print("no it didn't work")
+  
+}
